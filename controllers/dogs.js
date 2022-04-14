1f8bb51ed2f8dcab3d337323dde19fbb883c2e76
srikanth <s546826@nwmissouri.edu>
2022-04-14 18:12:54 -0500

    added

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -70,3 +70,35 @@ exports.dogs_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+exports.dogs_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await dogs.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+
+exports.dogs_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await dogs.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Dog_breed)  
+               toUpdate.Dog_breed = req.body.Dog_breed; 
+        if(req.body.age) toUpdate.age = req.body.age; 
+        if(req.body.Dog_name) toUpdate.Dog_name = req.body.Dog_name; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
+ 
